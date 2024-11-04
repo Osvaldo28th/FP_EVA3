751a6d8bcbe5f0ca7b7d0a32601deadd1bb8edd7
Osvaldo28th <L24550306@chihuahua2.tecnm.mx>
2024-11-04 16:50:50 -0600

    Add files via upload

---
+++ EVA3_1_METODOS.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_1_metodos;
+
+/**
+ *
+ * @author Osvaldo Chavez
+ */
+public class EVA3_1_METODOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //INVOCAR EL METODO, LLAMAR AL METODO --> USARLO
+        imprimirMensaje("Hola mundo");
+        
+        
+    }
+    //Declaracion del metodo
+    //Modificadores (opcionales)
+            //Valor retorno
+                //Nombre
+                                //Parametros (opcionales)
+    //SON UTILES PARA REAUTILIZAR CÓDIGO
+    public static void imprimirMensaje(String mensaje){
+        // Llaves cuerpo del metodo
+        System.out.println("Tu mensaje:" + " " + mensaje);
+    
+        
+    }
+    
+    
+    
+}
